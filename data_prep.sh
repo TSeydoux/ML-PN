58a9c32bd89bb93146a5711cb0984dcd057a8231
TSeydoux <72605796+TSeydoux@users.noreply.github.com>
2026-01-17 23:10:55 +0100

    Updated description
    I added a more complete description of the file content.

--- data_prep.sh
+++ data_prep.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+### Script to run 'data_prep.py'
+
 source /afs/cern.ch/work/t/thseydou/public/Miniconda/etc/profile.d/conda.sh    # Activates conda
 conda activate weaver                                                          # Activates the specific conda environment
 python /afs/cern.ch/work/t/thseydou/public/PN/data_prep.py                     # Runs the data_prep.py code
